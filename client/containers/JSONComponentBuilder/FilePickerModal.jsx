d0c4b462089fbac35b7c49083985945c0b4a5506
dvv.avinash <dvv.avinash@gmail.com>
2018-08-16 16:24:35 +0530

    styles for file uploader

--- client/containers/JSONComponentBuilder/FilePickerModal.jsx
+++ client/containers/JSONComponentBuilder/FilePickerModal.jsx
@@ -1,15 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
 
-import { map } from 'lodash';
-
-import { Modal, Image, Form, Button } from 'unchained-ui-react';
-import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import { map, debounce, filter, escapeRegExp } from 'lodash';
+import { Modal, Image, Form, Button, Input, Loader, Dimmer } from 'unchained-ui-react';
 
 import { getImages, uploadImage } from 'api/auth';
 
-// import 'react-tabs/style/react-tabs.less';
-import './ImagePickerModal.scss';
+import './FilePickerModal.scss';
 
 const data = {
   meta: {
@@ -21,8 +19,8 @@ const data = {
       meta: {
         type: 'wagtailimages.Image',
         detail_url: 'http://localhost/api/v2/images/22/',
-        tags: [],
-        file: 'https://dummyimage.com/100x100/e8d6e8/000fde.png&text=Avinash+600x315',
+        tags: ['avinash', 'super', 'sup'],
+        file: 'https://dummyimage.com/100x100/e8d6e8/000fde.png&text=avinash,super,sup',
       },
       title: 'test image2'
     },
@@ -31,8 +29,8 @@ const data = {
       meta: {
         type: 'wagtailimages.Image',
         detail_url: 'http://localhost/api/v2/images/23/',
-        tags: [],
-        file: 'https://dummyimage.com/100x100/e8d6e8/000fde.png&text=Alchemy+600x315',
+        tags: ['sup', 's', 'su'],
+        file: 'https://dummyimage.com/100x100/e8d6e8/000fde.png&text=sup,s,su',
       },
       title: 'Screen Shot 2018-07-19 at 10.33.58 AM.png'
     }
@@ -45,11 +43,18 @@ class FilePickerModal extends React.Component {
     updateImage: PropTypes.func,
   };
 
+  TAB_TYPES = {
+    FILE_UPLOAD: 'FILE_UPLOAD',
+    FILE_PREVIEW: 'FILE_PREVIEW'
+  };
+
   constructor(props) {
     super(props);
     this.state = {
       options: [],
-      images: data,
+      images: data.items,
+      tab: this.TAB_TYPES.FILE_PREVIEW,
+      isLoading: false,
       formData: {
         tags: [],
       },
@@ -60,11 +65,45 @@ class FilePickerModal extends React.Component {
     this.fetchImages();
   }
 
+  changeTab = (tab) => {
+    this.setState({
+      ...this.state,
+      tab,
+      images: data.items,
+    });
+  }
+
   fetchImages = async () => {
     const res = await getImages();
     this.setState({
       images: res,
     });
+  }
+
+
+  hidePop = () => {
+    this.props.handleModal(false);
+  }
+
+  handleSearchChange = (e, { value }) => {
+    this.setState({ isLoading: true });
+
+    setTimeout(() => {
+      if (value.length < 1) {
+        this.setState({
+          images: data.items,
+          isLoading: false,
+        });
+        return;
+      }
+      const re = new RegExp(escapeRegExp(value), 'i');
+      const isMatch = ({ meta: { tags } }) => filter(tags, tag => re.test(tag)).length > 0;
+      const images = filter(data.items, isMatch);
+      this.setState({
+        isLoading: false,
+        images,
+      });
+    }, 300);
   }
 
   onSubmit = async (e) => {
@@ -112,32 +151,58 @@ class FilePickerModal extends React.Component {
             }
           }}
         />
-        <Button type="submit">Submit</Button>
+        <div className="text-right">
+          <Button type="submit">Submit</Button>
+        </div>
       </Form>
     );
   }
-  hidePop = () => {
-    this.props.handleModal(false);
-  }
+
   render() {
     const { updateImage } = this.props;
-    const { images } = this.state; // eslint-disable-line
+    const { images, tab, isLoading } = this.state; // eslint-disable-line
     return (
       <div>
         <Modal open className="image-picker-modal" onClose={this.hidePop}>
           <Modal.Header>
-            Change Image
+            <Button className={classNames('tab', { active: tab === this.TAB_TYPES.FILE_PREVIEW })} onClick={() => this.changeTab(this.TAB_TYPES.FILE_PREVIEW)}>
+              Preview Files
+            </Button>
+            <Button className={classNames('tab', { active: tab === this.TAB_TYPES.FILE_UPLOAD })} onClick={() => this.changeTab(this.TAB_TYPES.FILE_UPLOAD)}>
+              Upload File
+            </Button>
           </Modal.Header>
-          <Modal.Content>
-            <Tabs>
-              <TabList>
-                <Tab>Pick Image </Tab>
-                <Tab>Upload Image </Tab>
-              </TabList>
-              <TabPanel>
+          <Modal.Content
+            className={
+              classNames({
+                active_tab_1: tab === this.TAB_TYPES.FILE_PREVIEW,
+                active_tab_2: tab === this.TAB_TYPES.FILE_UPLOAD
+              })}
+          >
+            {
+              tab === this.TAB_TYPES.FILE_PREVIEW &&
+              <div>
+                <Input
+                  category
+                  fluid
+                  loading={isLoading}
+                  icon="search"
+                  placeholder="Search..."
+                  onChange={debounce(this.handleSearchChange, 500, { leading: true })}
+                />
+                <br />
+                {
+                  isLoading &&
+                  <Dimmer active inverted>
+                    <Loader inverted content="Loading" />
+                  </Dimmer>
+                }
+                {
+                  !isLoading && images.length === 0 && <h1> No files are found </h1>
+                }
                 <Image.Group size="small">
                   {
-                    data.items.map((imageDetails) => {
+                    !isLoading && images.map((imageDetails) => {
                       const { meta: { file }, title } = imageDetails;
                       return (
                         <Image
@@ -151,11 +216,11 @@ class FilePickerModal extends React.Component {
                     })
                   }
                 </Image.Group>
-              </TabPanel>
-              <TabPanel>
-                {this.renderFileUploader()}
-              </TabPanel>
-            </Tabs>
+              </div>
+            }
+            {
+              tab === this.TAB_TYPES.FILE_UPLOAD && this.renderFileUploader()
+            }
           </Modal.Content>
         </Modal>
       </div>
